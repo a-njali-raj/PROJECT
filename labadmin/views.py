a45ea0c45c76e2e0b64fdbd4ae7df0e9ad92435b
Anjali Raj <anjaliraj2024a@mca.ajce.in>
2023-10-07 21:03:34 +0530

    commit

--- labadmin/views.py
+++ labadmin/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-
+from tests.models import User 
 @login_required
 def staff_dashboard(request):
     if request.user.is_staff and not request.user.is_superuser:
+        
         return render(request, "staff_dashboard.html")
     return redirect("home")
 
 @login_required
 def admin_dashboard(request):
     if request.user.is_superuser:
-        return render(request, "admin_dashboard.html")
+        users = User.objects.exclude(is_superuser=True)# Query the registered users
+        return render(request, "admin_dashboard.html", {"users": users})  # Pass 'users' to the template context
     return redirect("home")
 
